47e51aafe6eb81eef3b5316ea1d5dfba7f55d9a3
JuniorReis-dev <cleiton.nr47@gmail.com>
2025-04-16 20:25:40 -0300

    Live Code - Aula 04 -✨ Live Code - Laços de Repetição

--- aula_003/src/aula_003/Caracteres.java
+++ aula_003/src/aula_003/Caracteres.java
@@ -3,11 +3,25 @@ package aula_003;
 public class Caracteres {
 
 	public static void main(String[] args) {
-		String frase_1 = "turma java 82";
-		String frase_2 = "Turma Java 82";
-		String frase_3 = "turma java 82";
+		String frase_01 = "Turma Java 82";
+		String frase_02 = "TURMA JAVA 82";
+		String frase_03 = "TURMA JAVA 82";
 		
-		System.out.println(frase_1);
+		// Comparação usando operador
+		System.out.println(frase_01 == frase_02);
+		System.out.println(frase_03 == frase_02);
+		
+		// Comparação usando o método equals
+		System.out.println(frase_01.equals(frase_02));
+		System.out.println(frase_03.equals(frase_02));
+		
+		// Comparação usando o método equalsIgnoreCase
+		System.out.println(frase_01.equalsIgnoreCase(frase_02));
+		System.out.println(frase_03.equalsIgnoreCase(frase_02));
+		
+		// Comparação usando o método compareTo
+		System.out.println(frase_01.compareToIgnoreCase(frase_02));
+		System.out.println(frase_03.compareTo(frase_02));
 	}
 
 }
--- aula_003/src/aula_003/ConversorDeMoedas.java
+++ aula_003/src/aula_003/ConversorDeMoedas.java
@@ -6,7 +6,8 @@ public class ConversorDeMoedas {
 
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
-		
+		double valorConvertido = 0.0;
+		String moeda = "";
 		System.out.println("*************************************");
 		System.out.println("Conversor de moedas");
 		System.out.println("*************************************");
@@ -15,12 +16,33 @@ public class ConversorDeMoedas {
 		System.out.println("2- converter para pesos($)");
 		System.out.println("*************************************");
 		System.out.println("Digite a opçao desejada: ");
-		
 		int opcao = leia.nextInt();
+		System.out.println("Digite o valor em Reais (R$)");
+		double valor = leia.nextDouble();
+				
+		switch(opcao) {
+		case 1:
+			valorConvertido = valor / 5.86;
+			moeda = "Dólar";
+			break;
+		case 2:
+			valorConvertido = valor / 6.63;
+			moeda = "Euro";
+			break;
+		case 3:
+			valorConvertido = valor / 0.0049;
+			moeda = "Peso Argentino";
+			break;
+		default:
+			System.out.println("Opção inválida!");
+			leia.close();
+			return; // Sair do programa
+		}
 		
+		System.out.printf("Valor convertido em %s: %.2f", moeda, valorConvertido);
+		 
+		leia.close();
 		
-		
-
 	}
 
 }
